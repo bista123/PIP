83de0da796397897572f434b23115935260183aa
bista123 <jakub.bista@gmail.com>
2020-05-22 01:46:37 +0200

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -8,6 +8,7 @@ import random
 from scipy.spatial import distance
 from scipy.stats import levy
 from sklearn.cluster import KMeans
+from sklearn import datasets
 from scipy.spatial.distance import pdist
 
 
@@ -15,6 +16,7 @@ from scipy.spatial.distance import pdist
 #     'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
 #     'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
 # })
+from sklearn.metrics import davies_bouldin_score
 
 
 class Nest:
@@ -91,18 +93,32 @@ def main():
         'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
     })
 
+    # df = pd.read_excel('Wine.xlsx', sheet_name='Data')
+    #
+    # kmeans = KMeans(n_clusters=3, init='random', max_iter=10, n_init=1, random_state=0)
+    # y_kmeans = kmeans.fit_predict(df)
+    # print(y_kmeans)
+    # np.savetxt(r'result.txt', y_kmeans, fmt='%d')
+    # labels = kmeans.labels_
+    # print(davies_bouldin_score(df, labels))
+    # return
+    colmap = {1: 'r', 2: 'g', 3: 'b'}
+
     k = 3
+    pop = 3
     Beta = 1.5 #integrating clanok
     step_size = 1 #CS clanok
     Pa = 0.25 #sanca na najdenie cudzich vajicok v hniezde - integrating clanok
     max_gen = 10
+    stopping = 0
+    same = 0
     dimensions = len(df.columns)
     cl = list(df.columns.values)
     cols = cl.copy()
 
     # Vytvor pociatocne hniezda, inicializuj ich centroidy a pridaj ich do zoznamu hniezd
     nests = []
-    for i in range(k):
+    for i in range(pop):
         nest = Nest([], [], [], 0)
         nest.set_centroids(df, k)
         nests.append(nest)
@@ -113,41 +129,68 @@ def main():
         nest_df = assignment(d_frame, nest.nest_centroids, dimensions)
         nest.nest_clusters = nest_df['closest']
         nest.closest_distance = nest_df['min_dist']
-        # print(nest_df['min_dist'].mean())
         nest.fitness = nest_df['min_dist'].mean()
 
     # Uloz najlepsie hniezdo
     nests.sort(key=lambda x: x.fitness)
-    best_nest = nests[0]
+    best_nest = copy.deepcopy(nests[0])
+
+    # Uloz list hniezd kvoli overeniu stopping criteria
+    old_nests = copy.deepcopy(nests)
 
     # Inicializacia Levyho letu
-    new_nest = Nest([], [], [], 0)
-    new_nest.set_centroids(df, k)
+    # levy_nest = Nest([], [], [], 0)
+    # levy_nest.set_centroids(df, k)
 
     # Vstup do hlavne cyklu algoritmu - prechod hniezdami
     for itr in range(max_gen):
 
-        # Nechaj kukucku Lévyho letom najst nove hniezdo
-        # new_nest = copy.deepcopy(nest)
-        step = levy_step(Beta, dimensions) * step_size
-        for key, value in new_nest.nest_centroids.items():
-            new_nest.nest_centroids[key] = value + step
+        # # Nechaj kukucku Lévyho letom najst nove hniezdo
+        # # new_nest = copy.deepcopy(nest)
+        # step = levy_step(Beta, dimensions) * step_size
+        # for key, value in levy_nest.nest_centroids.items():
+        #     levy_nest.nest_centroids[key] = value + step
 
-        # Prirad prvky klastrom a vypocitaj fitness noveho hniezda
-        d_frame = copy.deepcopy(df)
-        new_nest_df = assignment(d_frame, new_nest.nest_centroids, dimensions)
-        new_nest.nest_clusters = new_nest_df['closest']
-        new_nest.fitness = new_nest_df['min_dist'].mean()
+        for i in range(0, len(nests)):
+            levy_nest = copy.deepcopy(nests[i])
+            step = levy_step(Beta, dimensions) * step_size
+            for key, value in levy_nest.nest_centroids.items():
+                levy_nest.nest_centroids[key] = value + step
 
-        # Vyber nahodne hniezda z pola
-        position = random.randint(0, len(nests) - 1)
-        old_nest = copy.deepcopy(nests[position])
+            # Prirad prvky klastrom a vypocitaj fitness noveho hniezda
+            d_frame = copy.deepcopy(df)
+            new_nest_df = assignment(d_frame, levy_nest.nest_centroids, dimensions)
+            levy_nest.nest_clusters = new_nest_df['closest']
+            levy_nest.closest_distance = new_nest_df['min_dist']
+            levy_nest.fitness = new_nest_df['min_dist'].mean()
 
-        # Ak ma nove hniezdo lepsiu fitness ako to stare nahrad ho
-        # Ak nove hniezdo nema mensiu fitness ako to stare nic sa nestane
-        if new_nest.fitness < old_nest.fitness:
-            nests.pop(position)
-            nests.insert(position, new_nest)
+            # Vyber nahodne hniezda z pola
+            position = random.randint(0, len(nests) - 1)
+            old_nest = copy.deepcopy(nests[position])
+
+            # Ak ma nove hniezdo lepsiu fitness ako to stare nahrad ho
+            # Ak nove hniezdo nema mensiu fitness ako to stare nic sa nestane
+            if levy_nest.fitness < old_nest.fitness:
+                nests.pop(position)
+                nests.insert(position, levy_nest)
+
+        # Zorad hniezda podla fitness a uloz najlepsie
+        nests.sort(key=lambda x: x.fitness)
+        best_nest = copy.deepcopy(nests[0])
+
+        # Zahod hniezda na zaklade pravdepodonosti Pa okrem najlepsieho, ak si hniezdo zahodil nahodne vygeneruj nove
+        for i in range(1, len(nests)):
+            probability = random.randint(0, 100)
+            if probability < Pa*100:
+                nests.pop(i)
+                new_nest = Nest([], [], [], 0)
+                new_nest.set_centroids(df, k)
+                d_frame = copy.deepcopy(df)
+                new_nest_df = assignment(d_frame, new_nest.nest_centroids, dimensions)
+                new_nest.nest_clusters = new_nest_df['closest']
+                new_nest.closest_distance = new_nest_df['min_dist']
+                new_nest.fitness = new_nest_df['min_dist'].mean()
+                nests.insert(i, new_nest)
 
         # Prepocitaj centroidy a prirad pozorovania k novym centroidom pre vsetky hniezda
         for nest in nests:
@@ -162,19 +205,163 @@ def main():
                         if nest.nest_clusters[m] == i:
                             counter += 1
                             suma += df[j][m]
-                    nest.nest_centroids[i][num] = suma/counter
+                    if suma != 0:
+                        nest.nest_centroids[i][num] = suma/counter
                     num += 1
 
             # Priradenie pozorovani k centroidom
             d_frame = copy.deepcopy(df)
             new_nest_df = assignment(d_frame, nest.nest_centroids, dimensions)
             nest.nest_clusters = new_nest_df['closest']
+            nest.closest_distance = new_nest_df['min_dist']
             nest.fitness = new_nest_df['min_dist'].mean()
-            print('l')
 
+        # Zorad hniezda podla fitness a uloz najlepsie
+        nests.sort(key=lambda x: x.fitness)
+        best_nest = copy.deepcopy(nests[0])
+
+        # Porovnaj fitness stareho a noveho listu hniezd
+        fit_dist = 0
+        for nest_old, nest_new in zip(old_nests, nests):
+            if nest_old.fitness == nest_new.fitness:
+                fit_dist += 1
+            else:
+                break
+
+        if fit_dist == pop:
+            same = 1
+        else:
+            same = 0
+
+        if same == 1:
+            stopping += 1
+        else:
+            stopping = 0
+
+        # Stopping kriterium - moje je ked 3 krat po sebe sa nezmeni fitness
+        if stopping == 3:
+            break
+
+    np.savetxt(r'result1.txt', best_nest.nest_clusters, fmt='%d')
+    print(davies_bouldin_score(df, best_nest.nest_clusters))
+
+    df['color'] = best_nest.nest_clusters.map(lambda x: colmap[x])
+    fig = plt.figure(figsize=(5, 5))
+    plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+    for i in best_nest.nest_centroids.keys():
+        plt.scatter(*best_nest.nest_centroids[i], color=colmap[i])
+    plt.xlim(0, 80)
+    plt.ylim(0, 80)
+    plt.show()
 
     return
-    
-   
+
+
+
+
+
+    # Read data
+    # df = pd.read_excel('Iris.xlsx', sheet_name='Data')
+
+    # Toto staci na obycajny k-means
+    # kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    # y_kmeans = kmeans.fit_predict(df)
+    # print(y_kmeans)
+
+
+    # max_df = max(pd.DataFrame.max(df, axis=0)) * 1.1
+    # min_df = min(pd.DataFrame.min(df, axis=0)) * 0.9
+    #
+    # cl = list(df.columns.values)
+    # cols = cl.copy()
+    # last = str(cl.pop())
+
+
+    # centroids = {
+    #     i + 1: [random.uniform(min_df, max_df) for j in range(len(df.columns))]
+    #     for i in range(k)
+    # }
+
+    # centroids = {1: [2,2,2,2], 2: [4,4,4,4], 3: [6,6,6,6]}
+    #
+    # print(centroids)
+
+    # centroids = {[]}
+
+    # fig = plt.figure(figsize=(5, 5))
+    # plt.scatter(df['x'], df['y'], color='k')
+    # colmap = {1: 'r', 2: 'g', 3: 'b'}
+    # for i in centroids.keys():
+    #     plt.scatter(*centroids[i], color=colmap[i])
+    # plt.xlim(0, 80)
+    # plt.ylim(0, 80)
+    # plt.show()
+
+
+    # fig = plt.figure(figsize=(5, 5))
+    # plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+    # for i in centroids.keys():
+    #     plt.scatter(*centroids[i], color=colmap[i])
+    # plt.xlim(0, 80)
+    # plt.ylim(0, 80)
+    # plt.show()
+
+
+    # Update Stage
+    # old_centroids = copy.deepcopy(centroids)
+
+    # def update(k, cols):
+    #     x = 0
+    #
+    #     for i in centroids.keys():
+    #         # print(i)
+    #         for j in cols:
+    #             centroids[i][x] = np.mean(df[df['closest'] == i][j])
+    #             # print(j)
+    #             # print(np.mean(df[df['closest'] == i][j]))
+    #             x += 1
+    #         x = 0
+    #     return k
+    #
+    # centroids = update(centroids, cols)
+
+
+    # fig = plt.figure(figsize=(5, 5))
+    # ax = plt.axes()
+    # plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+    # for i in centroids.keys():
+    #     plt.scatter(*centroids[i], color=colmap[i])
+    # plt.xlim(0, 80)
+    # plt.ylim(0, 80)
+    # plt.show()
+
+
+    # while True:
+    #     closest_centroids = df['closest'].copy(deep=True)
+    #
+    #     centroids = update(centroids, cols)
+    #     df = df.loc[:, :last]
+    #     df = assignment(df, centroids, dimensions)
+    #
+    #     # fig = plt.figure(figsize=(5, 5))
+    #     # plt.scatter(df['x'], df['y'], color=df['color'], alpha=0.5, edgecolor='k')
+    #     # for i in centroids.keys():
+    #     #     plt.scatter(*centroids[i], color=colmap[i])
+    #     # plt.xlim(0, 80)
+    #     # plt.ylim(0, 80)
+    #     # plt.show()
+    #
+    #
+    #     if closest_centroids.equals(df['closest']):
+    #         df = df.iloc[:, -1]
+    #         print(df)
+    #         # output_file = open("result.txt", "w")
+    #         # output_file.writelines(df)
+    #         # output_file.close()
+    #         np.savetxt(r'result.txt', df.values, fmt='%d')
+    #         break
+
 if __name__ == '__main__':
     main()
+
+
